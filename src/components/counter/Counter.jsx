e2436447e4096ce2c2a924c7f707a21bb1e4db53
qumengdie <qu_mengdie@126.com>
2022-07-15 01:41:15 -0400

    Finished the counter

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -53,37 +53,16 @@ class CounterButton extends Component {
     //state => counter 0
     constructor() {
         super();
-        this.state = {
-            counter : 0
-        }
-        this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
     } 
 
 
     render() {
         return (
             <div className="counter">
-                <button onClick={this.increment}>+{this.props.by}</button>
-                <button onClick={this.decrement}>-{this.props.by}</button> 
+                <button onClick={() => this.props.incrementMethod(this.props.by)}>+{this.props.by}</button>
+                <button onClick={() => this.props.decrementMethod(this.props.by)}>-{this.props.by}</button> 
             </div>
         )
-    }
-
-    increment() {  //Update state
-        this.setState({
-            counter : this.state.counter + this.props.by
-        });
-
-        this.props.incrementMethod(this.props.by);
-    }
-
-    decrement() {  //Update state
-        this.setState({
-            counter : this.state.counter - this.props.by
-        });
-
-        this.props.decrementMethod(this.props.by);
     }
     
 }
