4849fce0460551e0b4b3158438b3a7b823b7b147
qumengdie <qu_mengdie@126.com>
2022-07-29 05:17:12 -0400

    Refactored to common change event

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,3 +1,4 @@
+import { toHaveDisplayValue } from "@testing-library/jest-dom/dist/matchers";
 import React, {Component} from "react";
 
 class TodoApp extends Component {
@@ -17,25 +18,19 @@ class LoginComponent extends Component {
             username: 'qumengdie',
             password: ''
         }
-        this.handleUserNameChange = this.handleUserNameChange.bind(this)
-        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
 
-    handleUserNameChange(event) {
-        console.log(event.target.value);
-        this.setState({username: event.target.value})
-    }
-
-    handlePasswordChange(event) {
-        console.log(event.target.value);
-        this.setState({password: event.target.value})
+    handleChange(event) {
+        console.log(this.state);
+        this.setState({[event.target.name]: event.target.value})
     }
 
     render() {
         return (
             <div>
-                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleUserNameChange}/>
-                Password: <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange}/>
+                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+                Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
                 <button>login</button>
             </div>
         )
