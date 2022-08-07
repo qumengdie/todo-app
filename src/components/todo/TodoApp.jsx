555ba5f90d526ff16d0bf05e63d95cc46152d9f5
qumengdie <qu_mengdie@126.com>
2022-08-07 19:22:39 -0400

    Using bootstrap to create a menu

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import withNavigation from './WithNavigation.jsx'
 import withParams from './WithParams.jsx';
 import '/Users/qumengdie/Downloads/Projects/Todo/todo-app/src/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
 
 class TodoApp extends Component {
     render() {
@@ -30,9 +31,19 @@ class TodoApp extends Component {
 class HeaderComponent extends Component {
     render() {
         return (
-            <div>
-                Header <hr/>
-            </div>
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div><a className='navbar-brand'>QMD</a></div>
+                    <ul className="navbar-nav">
+                        <li className="nav-link"><Link className="nav-link" to="/welcome/qumengdie">Home</Link></li>
+                        <li className="nav-link"><Link className="nav-link" to="/todos">Todos</Link></li>
+                    </ul>
+                    <ul className="navbar-nav navbar-collapse justify-content-end">
+                        <li className="nav-link"><Link className="nav-link" to="/login">Login</Link></li>
+                        <li className="nav-link"><Link className="nav-link" to="/logout">Logout</Link></li>
+                    </ul>
+                </nav>
+            </header>
         )
     }
 }
@@ -100,7 +111,7 @@ class WelcomeComponent extends Component {
 }
 
 function ErrorComponent() {
-        return <div>Erorr!</div>
+        return <div>Erorr! This is an invalid page!</div>
 }
 
 class LoginComponent extends Component {
