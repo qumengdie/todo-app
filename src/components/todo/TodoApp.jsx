7f2556186109f993c4f077c26e838a058e1e8633
qumengdie <qu_mengdie@126.com>
2024-01-21 20:40:46 -0500

    Add a button to welcome page, comment counter.css for now

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import withNavigation from './WithNavigation.jsx'
 import withParams from './WithParams.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
