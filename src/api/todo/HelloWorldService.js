ab3ca4c46941bbc7cc30acfbc7ec22d34d96e127
qumengdie <qu_mengdie@126.com>
2024-01-24 01:11:25 -0500

    Connecting React with Restful API using Axios

---
+++ src/api/todo/HelloWorldService.js
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+class HelloWorldService {
+    excuteHelloWorldService() {
+        return axios.get('http://localhost:8080/hello-world')
+    }
+
+    excuteHelloWorldBeanService() {
+        return axios.get('http://localhost:8080/hello-world-bean')
+    }
+
+    excuteHelloWorldPathVariableService(name) {
+        return axios.get(`http://localhost:8080/hello-world/${name}`)
+    }
+}
+
+export default new HelloWorldService()
--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,10 +1,16 @@
 import React, {Component} from "react";
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import HelloWorldService from "../../api/todo/HelloWorldService";
 
 class WelcomeComponent extends Component {
     constructor(props) {
         super(props)
         this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this)
+        this.state = {
+            welcomeMessage : ''
+        }
+        this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this)
+        this.handleError = this.handleError.bind(this)
     }
 
     render() {
@@ -19,12 +25,32 @@ class WelcomeComponent extends Component {
                     <button onClick={this.retrieveWelcomeMessage}
                         className="btn btn-success">Get Welcome Message</button>
                 </div>
+                <div className='container'>
+                    {this.state.welcomeMessage}
+                </div>
             </>
         )
     }
 
     retrieveWelcomeMessage() {
-        console.log('retrieve clicked')
+        // HelloWorldService.excuteHelloWorldService()
+        // .then(response => this.handleSuccessfulResponse(response))
+
+        // HelloWorldService.excuteHelloWorldBeanService()
+        // .then(response => this.handleSuccessfulResponse(response))
+
+        HelloWorldService.excuteHelloWorldPathVariableService(this.props.params.name)
+        .then(response => this.handleSuccessfulResponse(response))
+        .catch(error => this.handleError(error))
+    }
+
+    handleSuccessfulResponse(response) {
+        this.setState({welcomeMessage : response.data.message})
+    }
+
+    handleError(error) {
+        console.log(error.response)
+        
     }
 }
 
